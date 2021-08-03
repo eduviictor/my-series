f0e6a36c5f5cf987e75eee32129d3e805191ee7d
eduviictor <eduvictornobrega@gmail.com>
2021-08-02 23:28:15 -0300

    test: ensure AddSeriesController calls AddSeries with correct values

--- src/presentation/controllers/add-series.spec.ts
+++ src/presentation/controllers/add-series.spec.ts
@@ -1,15 +1,36 @@
+import { SeriesModel } from '../../domain/models/series';
+import { AddSeries, AddSeriesModel } from '../../domain/usecases/add-series';
 import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
 import { AddSeriesController } from './add-series';
 
+const makeAddSeries = (): AddSeries => {
+  class AddSeriesStub implements AddSeries {
+    async add(series: AddSeriesModel): Promise<SeriesModel> {
+      const fakeSeries = {
+        id: 'valid_id',
+        name: 'valid_name',
+        description: 'valid_description',
+        score: 10,
+      };
+
+      return new Promise(resolve => resolve(fakeSeries));
+    }
+  }
+  return new AddSeriesStub();
+};
+
 interface SutTypes {
   sut: AddSeriesController;
+  addSeriesStub: AddSeries;
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AddSeriesController();
+  const addSeriesStub = makeAddSeries();
+  const sut = new AddSeriesController(addSeriesStub);
   return {
     sut,
+    addSeriesStub,
   };
 };
 
@@ -61,5 +82,23 @@ describe('AddSeries Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError('score'));
+  });
+
+  test('Should call AddSeries with correct values', async () => {
+    const { sut, addSeriesStub } = makeSut();
+    const addSpy = jest.spyOn(addSeriesStub, 'add');
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        description: 'any_description',
+        score: 10,
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      description: 'any_description',
+      score: 10,
+    });
   });
 });
